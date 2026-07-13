d8f3bf8dc6fa6901ec3953c0dff6ca35f0b46ed6
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-13 08:14:31 +0300

    chore:  add proxy config for api server and Manrope font family for typography

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite';
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [
     react(),
     svgr(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+});
