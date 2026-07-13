7dd2b124bac10bed96466a5c731d316339481d75
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-13 08:17:18 +0300

    feat: inital design for main layout

---
+++ src/layouts/StoreLayout.tsx
@@ -0,0 +1,12 @@
+import Steps from "../components/Steps";
+
+
+export default function StoreLayout() {
+  return (
+    <div className="w-full ">
+        {/* steps section  */}
+        <Steps />
+        {/* review section */}
+    </div>
+  )
+}
