4b9161f7006ebb46faf23e0aeaf473ba1ce924f4
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-13 08:15:34 +0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const headingText = `Let's get started!`;
+
+export default function Header() {
+  return (
+    <h1 className="text-[31.88px] leading-[110.00000000000001%] font-bold tracking-[-0.06px] h-8.75 md:hidden">
+      {headingText}
+    </h1>
+  );
+}
