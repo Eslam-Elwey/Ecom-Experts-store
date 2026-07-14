407545e00f706453ec63a320718637254b61542e
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-15 02:23:47 +0300

    feat: create counter component to count each selected product  => each variant has its own count

---
+++ src/components/Counter.tsx
@@ -0,0 +1,38 @@
+import Minus from "../assets/icons/minus.svg?react";
+import Plus from "../assets/icons/plus.svg?react";
+
+export default function Counter({
+  counter,
+  max,
+  setCount,
+}: {
+  counter: number;
+  max: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+}) {
+  return (
+    <div className="flex gap-2.5 justify-center items-center py-2.5">
+      <button
+        onClick={() => setCount((val: number) => val - 1)}
+        disabled={counter === 0}
+        className={`size-5 rounded-sm border-2 ${counter === 0 ? "border-[#E6EBF0] bg-[#FFFFFF]" : "border-[#F0F4F7] bg-[#F0F4F7]"}  flex items-center justify-center cursor-pointer`}
+      >
+        <Minus
+          className={`${counter !== 0 ? "text-[#525963]" : "text-[#CED6DE]"} size-2`}
+        />
+      </button>
+      <span className="font-medium text-[16px] leading-5 tracking-normal text-[#0B0D10]">
+        {counter}
+      </span>
+      <button
+        onClick={() => setCount((val: number) => val + 1)}
+        disabled={counter === max}
+        className={`size-5 rounded-sm border-2 ${counter === max ? "border-[#E6EBF0] bg-[#FFFFFF]" : "border-[#F0F4F7] bg-[#F0F4F7]"} flex items-center justify-center cursor-pointer`}
+      >
+        <Plus
+          className={` ${counter !== max ? "text-[#525963]" : "text-[#CED6DE]"} size-2`}
+        />
+      </button>
+    </div>
+  );
+}
