7fb9d81386028cffdfc4764e9a2ac4987aa3f968
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-16 00:44:39 +0300

    perf: remove logging

--- src/services/apiProducts.ts
+++ src/services/apiProducts.ts
@@ -3,13 +3,11 @@ import type { ProductType } from "../types/product.type";
 export async function getCategoryProdcuts(
   category: string,
 ): Promise<ProductType[]> {
-
   let endPoint = category.toLowerCase();
-  
+
   if (endPoint === "accessories") {
     endPoint = "protections";
   }
-  console.log(endPoint);
 
   try {
     const response = await fetch(`/api/${endPoint}`);
@@ -17,8 +15,8 @@ export async function getCategoryProdcuts(
       throw new Error(`error in fetching ${endPoint} data`);
     }
     const data = await response.json();
-    if(category==='plans') {
-        return data.items ;
+    if (category === "plans") {
+      return data.items;
     }
     return data;
   } catch (err) {
