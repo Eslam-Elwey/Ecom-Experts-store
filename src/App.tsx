8629cd8b4eb7e80de6d2cf759defc8888c97eb04
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-16 00:58:14 +0300

    feat: add cart context provider to wrap store layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
 import Header from "./components/Header";
+import CartContextProvider from "./contexts/CartContextProvider";
 import StoreLayout from "./layouts/StoreLayout";
 
 export default function App() {
   return (
     <div className="py-7.75 text-center md:py-12.5 md:w-10/12 md:mx-auto ">
       <Header />
-      <StoreLayout />
+      <CartContextProvider>
+        <StoreLayout />
+      </CartContextProvider>
     </div>
   );
 }
