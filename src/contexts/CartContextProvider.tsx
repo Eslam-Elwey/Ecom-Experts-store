e243a12cc1fc2580ba63d9d307af4bfae6f6d2c5
Eslam-Elwey <eslam.elwey@gmail.com>
2026-07-16 00:43:26 +0300

    feat: context provider to wrap all components within the context so can access it through its hook

---
+++ src/contexts/CartContextProvider.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import type { CartItemType } from "../types/cartItem.type";
+import { CartContext } from "./CartContext";
+
+export default function CartContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const STORAGE_KEY = "cart-items";
+
+  const [storedItems, setStoredItems] = useState<CartItemType[]>(() => {
+    const saved = localStorage.getItem(STORAGE_KEY);
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [isSaved, setIsSaved] = useState<boolean>(() => {
+    const saved = localStorage.getItem("isSaved");
+    return saved ? JSON.parse(saved) : false;
+  });
+
+  const addOrUpdateItem = (item: CartItemType) => {
+    setStoredItems((prev) => {
+      const index = prev.findIndex((ele) => {
+        return (
+          ele.productId === item.productId && ele.variantId === item.variantId
+        );
+      });
+
+      //creat new one
+      if (index === -1) {
+        return [...prev, item];
+      }
+
+      const updated = [...prev];
+
+      updated[index] = {
+        ...updated[index],
+        quantity: item.quantity,
+      };
+
+      return updated.filter((item) => item.quantity > 0);
+    });
+  };
+
+  function removeItem(productId: string, variantId: string) {
+    setStoredItems((prev) =>
+      prev.filter(
+        (item) =>
+          !(item.productId === productId && item.variantId === variantId),
+      ),
+    );
+  }
+
+  function getItemQuantity(productId: string, variantId: string) {
+    return (
+      storedItems.find(
+        (item) => item.productId === productId && item.variantId === variantId,
+      )?.quantity ?? 0
+    );
+  }
+
+  function clearAll() {
+    setStoredItems(() => []);
+  }
+
+  const subtotalBefore = storedItems.reduce(
+    (sum, item) =>
+      sum + (item.originalPrice ?? item.currentPrice) * item.quantity,
+    0,
+  );
+
+  const subtotalAfter = storedItems.reduce(
+    (sum, item) => sum + item.currentPrice * item.quantity,
+    0,
+  );
+
+  const savings = subtotalBefore - subtotalAfter;
+
+  useEffect(() => {
+    if (!isSaved) return;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(storedItems));
+  }, [storedItems, isSaved]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        addOrUpdateItem,
+        removeItem,
+        getItemQuantity,
+        clearAll,
+        items: storedItems,
+        setIsSaved,
+        isSaved,
+        savings,
+        subtotalAfter,
+        subtotalBefore,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
